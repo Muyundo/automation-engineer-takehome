55dc7554f28dd5cbb12adbfcc8b38b596f75d49c
MUYUNDO <muyundobrian@gmail.com>
2026-04-09 16:26:22 +0300

    add custom login commands to handle AUTH

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,10 +1,32 @@
-Cypress.Commands.add('BaseUrl', () => {
-  return 'https://automation-engineer-test.onrender.com/';
-});
+Cypress.Commands.add('login', (email, password) => {
+    // Visit the login page
+  cy.visit('/login')
 
-Cypress.Commands.add("login", (username, password) => {
-  cy.visit("/login");
-  cy.get("#username").type(username);
-  cy.get("#password").type(password);
-  cy.get("button[type=submit]").click();
-});
+  cy.get('[data-testid="login-email-input"]').clear().type(email)
+  cy.get('[data-testid="login-password-input"]').clear().type(password)
+
+  cy.get('[data-testid="login-submit-button"]').click()
+})
+
+
+// Custom commands for logging in as different user roles
+Cypress.Commands.add('loginAsRequester', () => {
+  cy.login(
+    Cypress.env('REQUESTER_EMAIL'),
+    Cypress.env('REQUESTER_PASSWORD')
+  )
+})
+
+Cypress.Commands.add('loginAsReviewer', () => {
+  cy.login(
+    Cypress.env('REVIEWER_EMAIL'),
+    Cypress.env('REVIEWER_PASSWORD')
+  )
+})
+
+Cypress.Commands.add('loginAsClaimer', () => {
+  cy.login(
+    Cypress.env('CLAIMER_EMAIL'),
+    Cypress.env('CLAIMER_PASSWORD')
+  )
+})
